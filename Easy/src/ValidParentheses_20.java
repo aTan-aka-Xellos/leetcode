46c89868a5a2a41f8ec134b10237a97753b895b0
aTan <atan.aka.xellos@gmail.com>
2018-10-26 02:17:42 +0300

    Add solution valid parentheses 20, ~99%

---
+++ Easy/src/ValidParentheses_20.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/valid-parentheses/
+ */
+public class ValidParentheses_20 {
+
+    public static boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : chars) {
+            if (c == '(' || c == '[' || c == '{') stack.push(c);
+
+            else if (stack.empty()) return false;
+
+            else if (c == ')' && stack.pop() != '(') return false;
+            else if (c == ']' && stack.pop() != '[') return false;
+            else if (c == '}' && stack.pop() != '{') return false;
+        }
+        return stack.size() == 0;
+    }
+
+    public static void main(String[] args) {
+        test(isValid("()"),     true);
+        test(isValid("(){}"),   true);
+        test(isValid("({[]})"), true);
+
+        test(isValid("["),    false);
+        test(isValid("}"),    false);
+        test(isValid("{]"),   false);
+        test(isValid("{[}]"), false);
+    }
+
+    private static void test(boolean actual, boolean expected) {
+        if (actual != expected) System.err.println(actual + " != " + expected);
+    }
+}
