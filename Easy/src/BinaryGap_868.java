f0bf419b442265c9a47f03299cd0e7caff285468
aTan <atan.aka.xellos@gmail.com>
2018-12-05 22:17:29 +0200

    add BinaryGap_868

---
+++ Easy/src/BinaryGap_868.java
@@ -0,0 +1,29 @@
+public class BinaryGap_868 {
+
+    public static void main(String[] args) {
+
+        assertEquals(binaryGap(5), 2);
+        assertEquals(binaryGap(6), 1);
+        assertEquals(binaryGap(8), 0);
+        assertEquals(binaryGap(22), 2);
+    }
+
+    public static int binaryGap(int N) {
+
+        char[] bits = Integer.toBinaryString(N).toCharArray();
+        int max = 0, counter = 0;
+
+        for (int i = 1; i < bits.length; i++) {
+            if (bits[i] == '0') counter++;
+            else {
+                max = Math.max(max, ++counter);
+                counter = 0;
+            }
+        }
+        return max;
+    }
+
+    private static void assertEquals(int actual, int expected) {
+        if (actual != expected) System.err.println(actual + " != " + expected);
+    }
+}
