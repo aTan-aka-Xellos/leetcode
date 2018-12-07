f204dcad599f4ccf1117b2cabc5b338f225489c2
aTan <atan.aka.xellos@gmail.com>
2018-12-07 02:00:37 +0200

    add BinarySearch_704

---
+++ Easy/src/BinarySearch_704.java
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode.com/explore/learn/card/binary-search/138/background/971
+ */
+public class BinarySearch_704 {
+
+    public static void main(String[] args) {
+        assertEquals(search(new int[]{-1,0,3,5,9,12}, 9), 4);
+        assertEquals(search(new int[]{-1,0,3,5,9,12}, 2), -1);
+    }
+
+    public static int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1, index;
+
+        do {
+            index = lo + (hi - lo) / 2;
+            if (nums[index] == target) return index;
+
+            if (nums[index] > target) hi = index - 1;
+            else                      lo = index + 1;
+        } while (lo <= hi);
+
+        return -1;
+    }
+
+    private static void assertEquals(int actual, int expected) {
+        if (actual != expected) System.err.println(actual + " != " + expected);
+    }
+}
