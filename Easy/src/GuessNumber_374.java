8beb4bddd0929872fb9644b1a6f4306b52a7caf3
aTan <atan.aka.xellos@gmail.com>
2018-12-08 15:31:05 +0200

    add GuessNumber_374

---
+++ Easy/src/GuessNumber_374.java
@@ -0,0 +1,47 @@
+import static java.lang.Math.abs;
+
+import java.util.Random;
+
+public class GuessNumber_374 {
+
+    private static final int RANDOM = abs(new Random().nextInt());
+    private static int count = 0;
+
+    public static void main(String[] args) {
+        int found = guessNumber(Integer.MAX_VALUE);
+
+        System.out.println();
+        System.out.println("Pick:  " + RANDOM);
+        System.out.println("Found: " + found);
+        System.out.println("Count: " + count);
+
+        assertEquals(found, RANDOM);
+    }
+
+    // TODO: has a bug, almost always use 30 guesses
+    public static int guessNumber(int n) {
+        int guess, mid, lo = 0, hi = n;
+
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            System.out.println(lo + " " + hi + " " + mid);
+            guess = guess(mid);
+            if (guess == 0) {
+                return mid;
+            }
+            if (guess > 0) lo = mid + 1;
+            else           hi = mid - 1;
+        }
+        return lo;
+    }
+
+    private static int guess(int guess) {
+        count++;
+        System.out.println("Guess: " + guess);
+        return Integer.compare(RANDOM, guess);
+    }
+
+    private static void assertEquals(int actual, int expected) {
+        if (actual != expected) System.err.println(actual + " != " + expected);
+    }
+}
