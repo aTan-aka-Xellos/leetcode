bd602665286d5f3a9a850867b57e778fc41883ca
aTan <atan.aka.xellos@gmail.com>
2018-12-08 22:01:08 +0200

    add firstBadVersion_simple

--- src/bs/easy/FirstBadVersion_278.java
+++ src/bs/easy/FirstBadVersion_278.java
@@ -11,31 +11,42 @@ public class FirstBadVersion_278 {
     public static void main(String[] args) {
 
         assertEquals(firstBadVersion(100), RANDOM);
+        assertEquals(firstBadVersion_simple(100), RANDOM);
 
         RANDOM = 1;
-        assertEquals(firstBadVersion(1), RANDOM);
+        assertEquals(firstBadVersion(1),  RANDOM);
         assertEquals(firstBadVersion(10), RANDOM);
+        assertEquals(firstBadVersion_simple(1),  RANDOM);
+        assertEquals(firstBadVersion_simple(10), RANDOM);
 
         RANDOM = 100;
-        assertEquals(firstBadVersion(100), RANDOM);
+        assertEquals(firstBadVersion(100),        RANDOM);
+        assertEquals(firstBadVersion_simple(100), RANDOM);
     }
 
     public static int firstBadVersion(int n) {
         int mid, lo = 1, hi = n;
-        boolean isBad;
 
         while (lo < hi) {
             mid = lo + (hi - lo) / 2;
-            isBad = isBadVersion(mid);
-
-            if (!isBad && isBadVersion(mid + 1)) return mid + 1;
-            else if (!isBad) lo = mid + 1;
+            if (!isBadVersion(mid) && isBadVersion(mid + 1)) return mid + 1;
+            else if (!isBadVersion(mid)) lo = mid + 1;
             else             hi = mid;
         }
         if (lo == hi && isBadVersion(lo)) return lo;
         return -1;
     }
 
+    public static int firstBadVersion_simple(int n) {
+        int left = 1, right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) right = mid;
+            else                   left = mid + 1;
+        }
+        return left;
+    }
 
     private static boolean isBadVersion(int n) {
         return n >= RANDOM;
