7742f807c3f9bae6d6802bad2889ba7dcad3f8ef
aTan <atan.aka.xellos@gmail.com>
2018-12-08 12:07:28 +0200

    add Sqrt_69

---
+++ Easy/src/Sqrt_69.java
@@ -0,0 +1,33 @@
+public class Sqrt_69 {
+
+    public static void main(String[] args) {
+        assertEquals(mySqrt(0), 0);
+        assertEquals(mySqrt(1), 1);
+        assertEquals(mySqrt(3), 1);
+        assertEquals(mySqrt(4), 2);
+        assertEquals(mySqrt(8), 2);
+        assertEquals(mySqrt(9), 3);
+        assertEquals(mySqrt(2147395599), 46339);
+        assertEquals(mySqrt(2147483647), 46340);
+    }
+
+
+    public static int mySqrt(int x) {
+
+        if (x == 0) return 0;
+        int mid, left = 1, right = x;
+
+        while (left < right) {
+            mid = left + (right - left) / 2;
+
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1)) return mid;
+            if (mid >  x / mid) right = mid;
+            else                left  = mid;
+        }
+        return left;
+    }
+
+    private static void assertEquals(int actual, int expected) {
+        if (actual != expected) System.err.println(actual + " != " + expected);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LeetCode
+
+To recall:
+ * Sqrt_69
