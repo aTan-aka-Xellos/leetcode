93cce9dd1e3ddec1a5650539946d4113ddc067b1
aTan <atan.aka.xellos@gmail.com>
2018-12-14 13:12:41 +0200

    add short version _twoSum()

--- src/arrays/easy/TwoSum_167.java
+++ src/arrays/easy/TwoSum_167.java
@@ -6,6 +6,15 @@ public class TwoSum_167 {
         twoSum(new int[]{2, 7, 11, 15}, 9);
     }
 
+    public int[] _twoSum(int[] a, int t) {
+        int i = 0, j = a.length - 1;
+        while  (a[j] != t - a[i]) {
+            if (a[j] <  t - a[i]) i++;
+            else                  j--;
+        }
+        return new int[]{++i, ++j};
+    }
+
     public static int[] twoSum(int[] numbers, int target) {
         int[] diff = new int[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
