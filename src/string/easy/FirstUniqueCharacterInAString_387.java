f12019b65206a450eb1a5b9563b077b0e0fd67e2
aTan <atan.aka.xellos@gmail.com>
2019-07-21 07:37:16 +0300

    add first unique char at string 387

--- src/string/easy/FirstUniqueCharacterInAString_387.java
+++ src/string/easy/FirstUniqueCharacterInAString_387.java
@@ -1,4 +1,28 @@
 package string.easy;
 
-public class FirstUniqueCharacterInAString_ {
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Time complexity: O(2N) = O(N)
+ */
+public class FirstUniqueCharacterInAString_387 {
+
+    public int firstUniqChar(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), -1);
+            } else {
+                map.put(s.charAt(i), i);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) >= 0) return i;
+        }
+        return -1;
+    }
 }
--- src/string/medium/ReverseWordsInString_151.java
+++ src/string/medium/ReverseWordsInString_151.java
@@ -2,14 +2,20 @@ package string.medium;
 
 import java.util.Stack;
 
+/**
+ * Useful things:
+ * - define reverse-in-place function for array with 3 arguments: (array, start, end)
+ * - reversing and clean-up spaces and be done in separate steps to simplify (but more code)
+ */
 public class ReverseWordsInString_151 {
 
     /**
-     * First accepted solution. Not the most optimal one due:
-     * - stack
-     * - trim
-     * - concatenation
-     */
+         * First accepted solution. Not the most optimal one due:
+         * - stack
+         * - trim
+         * - concatenation
+         */
+
     public String reverseWords(String s) {
 
         String result = "";
