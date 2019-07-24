f4f01a07ad9b2102651cacadbe07415e2161a2f8
aTan <atan.aka.xellos@gmail.com>
2019-07-24 10:33:37 +0300

    add TwoCityScheduling_1029

---
+++ src/greedy/easy/TwoCityScheduling_1029.java
@@ -0,0 +1,52 @@
+package greedy.easy;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/two-city-scheduling/submissions
+ * Time complexity: O(N logN + N) = O(N logN)
+ * Space complexity: O(1)
+ */
+public class TwoCityScheduling_1029 {
+
+    /**
+     * Execution time: ~40-50 ms
+     * The issue is lambda
+     */
+    public int twoCitySchedCost(int[][] costs) {
+
+        // Arrays.sort(costs, Comparator.comparingInt(a -> a[0] - a[1]));
+
+        // sort by difference in the schedule between A and B
+        Arrays.sort(costs, (a, b) -> (a[0] - a[1] - (b[0] - b[1])));
+
+        int sum = 0;
+
+        for (int i = 0; i < costs.length / 2; i++) {
+            sum += costs[i][0];
+            sum += costs[costs.length - 1 - i][1];
+        }
+
+        return sum;
+    }
+
+    /**
+     * https://leetcode.com/problems/two-city-scheduling/discuss/285099
+     * Execution time: 1 ms
+     */
+    public int _twoCitySchedCost(int[][] costs) {
+        int[] diff = new int[costs.length];
+        int sum = 0;
+        for(int i=0; i<costs.length; i++) {
+            diff[i] = costs[i][0] - costs[i][1];
+            sum += costs[i][0];
+        }
+        Arrays.sort(diff);
+        for(int i=diff.length/2; i<diff.length; i++) {
+            sum -= diff[i];
+        }
+        return sum;
+
+    }
+
+}
