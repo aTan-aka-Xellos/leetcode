dc23890bf237469bfd9a021bc49307a26405fdfc
aTan <atan.aka.xellos@gmail.com>
2019-07-27 17:58:28 +0300

    add ClimbingStairs_70 iterative

---
+++ src/others/easy/ClimbingStairs_70.java
@@ -0,0 +1,21 @@
+package others.easy;
+
+/**
+ * https://leetcode.com/problems/climbing-stairs
+ */
+public class ClimbingStairs_70 {
+
+    public int climbStairs(int n) {
+
+        if (n <= 1) return n;
+        int sum = 1, temp, prev = 1;
+
+        for (int i = 2; i <= n; i++) {
+            temp = sum;
+            sum = sum + prev;
+            prev = temp;
+        }
+        return sum;
+    }
+
+}
