87b99cd6144f1efbf686090dbb2dbdbc4d1fa1fe
aTan-aka-Xellos <atan.aka.xellos@gmail.com>
2020-06-16 15:22:01 +0900

    add solution 16-June

---
+++ june_challange/src/SearchInABinarySearchTree.java
@@ -0,0 +1,25 @@
+public class SearchInABinarySearchTree {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        while (root != null && root.val != val) {
+            if (val > root.val) root = root.right;
+            else root = root.left;
+        }
+
+        return root;
+    }
+
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+}
