e35e6d00a930669f296f90ca87fa1e9e931f7987
aTan-aka-Xellos <atan.aka.xellos@gmail.com>
2020-09-13 13:58:07 +0900

    add findDiagonalOrder_Dijkstra

--- src/arrays/medium/DiagonalTraverse_II_1424.java
+++ src/arrays/medium/DiagonalTraverse_II_1424.java
@@ -7,6 +7,40 @@ import java.util.*;
  */
 public class DiagonalTraverse_II_1424 {
 
+    public static void main(String[] args) {
+        List<List<Integer>> list = new ArrayList<>();
+        list.add(new ArrayList<Integer>(){{add(1); add(2);}});
+        list.add(new ArrayList<Integer>(){{add(3); add(4);}});
+
+        findDiagonalOrder_Dijkstra(list);
+    }
+
+    public static int[] findDiagonalOrder_Dijkstra(List<List<Integer>> nums) {
+
+        List<Integer> res = new ArrayList<>();
+        PriorityQueue<Integer[]> queue = new PriorityQueue<>(Comparator.<Integer[]>comparingInt(a -> a[0] + a[1]).thenComparingInt(a -> a[1]));
+
+        queue.add(new Integer[]{0, 0});
+
+        while (!queue.isEmpty()) {
+            Integer[] current = queue.poll();
+            int row = current[0];
+            int col = current[1];
+
+            if (nums.get(row).get(col) == Integer.MIN_VALUE) continue;
+
+            res.add(nums.get(row).get(col));
+            nums.get(row).set(col, Integer.MIN_VALUE);
+
+            if (row + 1 < nums.size() && col < nums.get(row + 1).size())
+                queue.add(new Integer[]{row + 1, col});
+
+            if (row < nums.size()     && col + 1 < nums.get(row).size())
+                queue.add(new Integer[]{row, col + 1});
+        }
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
 
         List<Integer> res = new ArrayList<>();
