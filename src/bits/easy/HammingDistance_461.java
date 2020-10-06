cfd138082b5415a17479f6ff60ce33814e9375bf
aTan-aka-Xellos <atan.aka.xellos@gmail.com>
2020-10-06 19:55:29 +0900

    add HammingDistance_461

---
+++ src/bits/easy/HammingDistance_461.java
@@ -0,0 +1,19 @@
+package bits.easy;
+
+/**
+ * https://leetcode.com/problems/hamming-distance/
+ */
+public class HammingDistance_461 {
+
+    public int hammingDistance(int x, int y) {
+        int z = x ^ y;
+        int count = 0;
+
+        while (z > 0) {
+            count += z & 1;
+            z >>= 1;
+        }
+        return count;
+    }
+
+}
