3551be839d1ea0268c5b0dfa1ccce73a25cc2cf2
aTan-aka-Xellos <atan.aka.xellos@gmail.com>
2020-10-23 10:55:05 +0900

    update Triangle_120

--- src/arrays/medium/Triangle_120.java
+++ src/arrays/medium/Triangle_120.java
@@ -9,7 +9,19 @@ import java.util.List;
  */
 public class Triangle_120 {
 
-    public int minimumTotal(List<List<Integer>> triangle) {
+    // https://leetcode.com/problems/triangle/discuss/38724/7-lines-neat-Java-Solution
+    public int minimumTotal_v2(List<List<Integer>> triangle) {
+
+        int[] dp = new int[triangle.size() + 1];
+        for (int i = triangle.size() - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+
+    public int minimumTotal_v1(List<List<Integer>> triangle) {
         List<List<Integer>> sum = new ArrayList<>();
 
         sum.add(Arrays.asList(triangle.get(0).get(0)));
