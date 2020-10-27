835965850e76d5c72a1db3593a403ea63cfaaa76
aTan-aka-Xellos <atan.aka.xellos@gmail.com>
2020-10-27 20:41:42 +0900

    re-solve NTreePreorderTraversal_589

---
+++ src/tree/easy/NTreePreorderTraversal_589.java
@@ -0,0 +1,48 @@
+package tree.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class NTreePreorderTraversal_589 {
+
+    // recursive
+    List<Integer> global = new ArrayList<>();
+
+    public List<Integer> preorder_v1(Node root) {
+        if (root == null) return global;
+
+        global.add(root.val);
+        for (Node node: root.children){
+            preorder_v1(node);
+        }
+        return global;
+    }
+
+
+    // iterative
+    public List<Integer> preorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+
+        if (root != null) stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            list.add(node.val);
+
+            for (int i = node.children.size() - 1; i >= 0; i--) {
+                stack.push(node.children.get(i));
+            }
+        }
+        return list;
+    }
+
+
+
+    static class Node {
+        public int val;
+        public List<Node> children;
+        public Node() {}
+    }
+}
