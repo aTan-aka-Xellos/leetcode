fbcf569634429e6c3d0340bf9d0324be48a462d6
aTan-aka-Xellos <atan.aka.xellos@gmail.com>
2021-02-03 14:05:58 +0900

    add KokoEatingBananas_875

---
+++ src/bs/medium/KokoEatingBananas_875.java
@@ -0,0 +1,31 @@
+package bs.medium;
+
+/**
+ * https://leetcode.com/problems/koko-eating-bananas/
+ */
+public class KokoEatingBananas_875 {
+
+    // 02/03/2021
+    // - confused what to return: L or R
+    // - confused < and <=
+    public int minEatingSpeed(int[] piles, int H) {
+        int L = 0, R = 1_000_000_000;
+
+        while (R - L > 1) {
+            int mid = (L + R) >>> 1;
+            if (canEat(mid, piles, H)) R = mid;
+            else                       L = mid;
+        }
+        return R;
+    }
+
+    boolean canEat(int eat, int[] piles, int H) {
+        int hours = 0;
+        for (int pile: piles) {
+            hours += (pile - 1) / eat + 1;
+        }
+        return hours <= H;
+    }
+
+
+}
