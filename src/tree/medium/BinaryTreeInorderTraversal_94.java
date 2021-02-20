6235605a3a35d3888301ba8ac9da4675b4ef0a60
aTan-aka-Xellos <atan.aka.xellos@gmail.com>
2021-02-20 21:11:30 +0900

    update BinaryTreeInorderTraversal_94

--- src/tree/medium/BinaryTreeInorderTraversal_94.java
+++ src/tree/medium/BinaryTreeInorderTraversal_94.java
@@ -1,12 +1,52 @@
 package tree.medium;
 
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
 public class BinaryTreeInorderTraversal_94 {
 
-    public List<Integer> inorderTraversal(TreeNode root) {
+    // 02/17/2021
+    public List<Integer> inorderTraversal_v2(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        traverse(root, list);
+        return list;
+    }
+
+    void traverse(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        traverse(node.left, list);
+        list.add(node.val);
+        traverse(node.right, list);
+    }
+
+    // 02/20/2021
+    // problematic part, need repetition
+    public List<Integer> inorderTraversal_v3(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            list.add(current.val);
+            current = current.right;
+        }
+        return list;
+    }
+
+
+    // 12/17/2018
+    public List<Integer> inorderTraversal_v1(TreeNode root) {
         List<Integer> list = new LinkedList<>();
         if (root == null) return list;
 
@@ -25,7 +65,7 @@ public class BinaryTreeInorderTraversal_94 {
         return list;
     }
 
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
