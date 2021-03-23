08085f27e1235cf85911a789213afc6b237edc8e
aTan-aka-Xellos <atan.aka.xellos@gmail.com>
2021-03-23 20:52:32 +0900

    update KClosestPointsToOrigin_973

--- src/heap/medium/KClosestPointsToOrigin_973.java
+++ src/heap/medium/KClosestPointsToOrigin_973.java
@@ -9,9 +9,25 @@ import java.util.PriorityQueue;
  */
 public class KClosestPointsToOrigin_973 {
 
-    // https://leetcode.com/problems/k-closest-points-to-origin/
+    // 03/23/2021
+    public int[][] kClosest(int[][] points, int k) {
+        int[][] ans = new int[k][2];
+        int[][] dist = new int[points.length][2];
 
-    public int[][] kClosest(int[][] points, int K) {
+        for (int i = 0; i < points.length; i++) {
+            int d = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            dist[i][0] = d;
+            dist[i][1] = i;
+        }
+
+        Arrays.sort(dist, Comparator.comparingInt(a -> a[0]));
+
+        for (int i = 0; i < k; i++) ans[i] = points[dist[i][1]];
+        return ans;
+    }
+
+    // 11/29/2020
+    public int[][] kClosest_v1(int[][] points, int K) {
         int[][] ans = new int[K][2];
 
         PriorityQueue<int[]> pq = new PriorityQueue<>(
