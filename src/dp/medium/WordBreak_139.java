530b6835d9bc61882b4f0534ff50a90ab9c33ec6
aTan-aka-Xellos <atan.aka.xellos@gmail.com>
2021-03-28 12:01:06 +0900

    update WordBreak_139

--- src/dp/medium/WordBreak_139.java
+++ src/dp/medium/WordBreak_139.java
@@ -1,6 +1,8 @@
 package dp.medium;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/word-break/
@@ -15,8 +17,32 @@ import java.util.List;
  */
 public class WordBreak_139 {
 
-    // 10/28/2020 +hint from Alex
+    // 28/03/2021
+    // O(20 * 20 * N)
+    // 20 - substring, 20 - contains, N - iterations
     public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        int max = 20;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j <= max && j <= i; j++) {
+                if (!dp[i - j]) continue;
+
+                String sub = s.substring(i - j, i);
+                if (set.contains(sub)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    // 10/28/2020 +hint from Alex
+    public boolean wordBreak_v1(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
 
